49febdd1c987dae27a18e228a65025c26558517c
Jesus Hugo Retamozo Leon <jesusretamozo@hotmail.it>
2024-08-31 14:25:01 +0200

    creado div para poner mis listas

--- React-Base/src/componenti/MainTotal/components/Logo/index.jsx
+++ React-Base/src/componenti/MainTotal/components/Logo/index.jsx
@@ -1,5 +1,10 @@
 import "./css/index.css";
 
+import * as React from "react";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { StaticDateTimePicker } from "@mui/x-date-pickers/StaticDateTimePicker";
+
 export default function Logo() {
   return (
     <>
@@ -7,7 +12,15 @@ export default function Logo() {
         <div>
           <h1 className="text-white">function logo desde main</h1>
         </div>
-
+        <div className=" flex flex-col items-center gap-5 ">
+          <div className=" flex flex-col items-center md:flex-row md:items-center m-8 gap-5 ">
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <StaticDateTimePicker orientation="portrait" />
+            </LocalizationProvider>
+            <div>hola dentro del div para crear tipo una dashboard</div>
+          </div>
+          <div>aca voy a poner mis lista creadas</div>
+        </div>
       </section>
     </>
   );
